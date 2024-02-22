b8e1212d2ec4b88855f4bb281e246a54d09671a8
shaulov <shineginn@gmail.com>
2024-02-22 18:01:06 +0300

    Add lexer class and its logic

--- computer-science/program-lang-creation/src/index.ts
+++ computer-science/program-lang-creation/src/index.ts
@@ -1,3 +1,12 @@
-const str: string = "ТЕСТ";
+import Lexer from "./lexer";
 
-console.log(str);
+const code = `
+  код РАВНО 5 ПЛЮС  9 ПЛЮС (4 МИНУС 6);
+  КОНСОЛЬ код;
+  переменная РАВНО код ПЛЮС 3;
+  КОНСОЛЬ переменная ПЛЮС код МИНУС 6;
+`;
+
+const lexer = new Lexer(code);
+lexer.lexAnalysis();
+console.log(lexer.tokenList);
