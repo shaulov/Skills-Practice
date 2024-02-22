e809b28f5af4383e2aa8146bfbd78c6237f4c020
shaulov <shineginn@gmail.com>
2024-02-22 18:00:15 +0300

    Change log operation name

--- computer-science/program-lang-creation/src/token-type.ts
+++ computer-science/program-lang-creation/src/token-type.ts
@@ -12,11 +12,11 @@ export const tokenTypesList = {
   'NUMBER': new TokenType('NUMBER', '[0-9]*'),
   'VARIABLE': new TokenType('VARIABLE', '[а-я]*'),
   'SEMICOLON': new TokenType('SEMICOLON', ';'),
-  'SPACE': new TokenType('SPACE', '[\\n\\t\\r]'),
+  'SPACE': new TokenType('SPACE', '[ \\n\\t\\r]'),
   'ASSIGN': new TokenType('ASSIGN', 'РАВНО'),
-  'LOG': new TokenType('LOG', 'ЛОГ'),
+  'LOG': new TokenType('LOG', 'КОНСОЛЬ'),
   'PLUS': new TokenType('PLUS', 'ПЛЮС'),
   'MINUS': new TokenType('MINUS', 'МИНУС'),
   'LPAR': new TokenType('LPAR', '\\('),
   'RPAR': new TokenType('RPAR', '\\)'),
-};
+}
